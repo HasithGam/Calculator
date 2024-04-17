b9de6994451b94e12cc692d4c533e85742b5e7cb
HasithGam <159763235+HasithGam@users.noreply.github.com>
2024-04-17 16:58:12 +1000

    170420240458h

---
+++ Calculator/scripts.js
@@ -0,0 +1,12 @@
+function displayValue(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    var result = eval(document.getElementById('display').value);
+    document.getElementById('display').value = result;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = "";
+}
